8f0dbf7acdfd20444a882c83107a85e487422cf0
kikio1101 <sadkop00@daum.net>
2022-10-17 13:12:05 +0900

    fix sqlite 시퀀스 문제 해결

--- server/src/products/products.controller.ts
+++ server/src/products/products.controller.ts
@@ -39,7 +39,7 @@ export class ProductsController {
   }
 
   @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.productsService.remove(+id);
+  delete(@Param('id') id: string) {
+    return this.productsService.delete(+id);
   }
 }
--- server/src/products/products.service.ts
+++ server/src/products/products.service.ts
@@ -26,7 +26,23 @@ export class ProductsService {
     return this.repo.update(id, updateProductDto);
   }
 
-  async remove(id: number) {
-    return this.repo.delete(id);
+  // sqlite에서 삭제된 데이터의 시퀀스가 남아있어 AUTO_INCREMENT 사용시 인덱스가 초기화가 안되는 문제 해결
+  resetSequence = async () => {
+    const reponse = await this.findAll();
+    if (reponse?.length === 0) {
+      await this.repo.query(
+        'UPDATE SQLITE_SEQUENCE SET seq = 0 WHERE name = "product"',
+      );
+    }
+  };
+
+  async delete(id: number) {
+    const excute = this.repo.delete(id);
+
+    excute.then(() => {
+      this.resetSequence();
+    });
+
+    return excute;
   }
 }
